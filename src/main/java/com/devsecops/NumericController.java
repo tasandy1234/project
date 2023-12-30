4cdef3207ec2ec0e96816081d654274d0a4608e6
tasandy1234 <147338835+tasandy1234@users.noreply.github.com>
2023-12-30 08:22:54 +0530

    Update NumericController.java

--- src/main/java/com/devsecops/NumericController.java
+++ src/main/java/com/devsecops/NumericController.java
@@ -22,7 +22,7 @@ public class NumericController {
 
 		@GetMapping("/")
 		public String welcome() {
-			return "Kubernetes DevSecOps by Capartech for testing";
+			return "Kubernetes Sandeep";
 		}
 
 		@GetMapping("/compare/{value}")
